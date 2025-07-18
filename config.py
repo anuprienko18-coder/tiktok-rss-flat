dbb01d17ecf4d445b924352718c377ed631e39b2
anuprienko18-coder <anuprienko18@gmail.com>
2025-07-18 22:36:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Set your GH Pages URL here e.g.:
 # Set your GH Pages URL here
-ghPagesURL = "https://<ваш_логин>.github.io/tiktok-rss-flat/"
-ghRawURL   = "https://raw.githubusercontent.com/<ваш_логин>/tiktok-rss-flat/main/"
+ghPagesURL = "https://anuprienko18-coder.github.io/tiktok-rss-flat/"
+ghRawURL   = "https://raw.githubusercontent.com/anuprienko18-coder/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
